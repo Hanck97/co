5be25ab92acd8cdbcedf06542ed7cb8469c82472
hanyang <hanyang@meili-inc.com>
2018-08-20 17:55:25 +0800

    完成co

---
+++ co.js
@@ -0,0 +1,17 @@
+function co(generator) {
+    return function(fn) {
+      var gen = generator();
+      function next(err, result) {
+          if(err){
+              return fn(err);
+          }
+          var step = gen.next(result);
+          if (!step.done) {
+              step.value(next);
+          } else {
+              fn(null, step.value);
+          }
+      }
+      next();
+     }
+  }
